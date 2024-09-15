75350ee48bcb73903fd22496a252fc6fffbb9e1c
Camilo Orocú <luiscamilooc@gmail.com>
2024-09-14 20:13:30 -0600

    Creando la clase de conexion con la BD y el modelo de empleado

---
+++ Progra2/Models/EmpleadoModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Progra2.Models
+{
+    public class EmpleadoModel
+    {
+        public int Id { get; set; }
+        public int IdPuesto { get; set; }
+        public string? NombrePuesto { get; set; }
+        [Required(ErrorMessage = "El campo ValorDocumentoIdentidad es obligatorio")]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Solo se permiten caracteres numéricos.")]
+        public string? ValorDocumentoIdentidad { get; set; }
+        [Required(ErrorMessage = "El campo Nombre es obligatorio")]
+        [RegularExpression(@"^[a-zA-ZáéíóúÁÉÍÓÚñÑ\- ]+$", ErrorMessage = "Solo se permiten caracteres alfabéticos")]
+        public string? Nombre { get; set; }
+        public DateTime FechaContratacion { get; set; }
+        public short SaldoVacaciones { get; set; }
+        public bool EsActivo { get; set; }
+    }
+}
