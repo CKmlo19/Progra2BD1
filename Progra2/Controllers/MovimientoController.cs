b43c5c0860203a82e36ee98f137e6b58cc41fce2
Camilo Orocú <luiscamilooc@gmail.com>
2024-09-22 00:14:20 -0600

    Creando el controller, el model y el data para movimientos asi como la vista

---
+++ Progra2/Controllers/MovimientoController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Progra2.Data;
+using Progra2.Models;
+
+namespace Progra2.Controllers
+{
+    public class MovimientoController : Controller
+    {
+        MovimientoData _movimientoData = new MovimientoData();
+        EmpleadoData _empleadoData = new EmpleadoData();
+        public IActionResult Listar(int id)
+        {
+            var oLista = _movimientoData.Listar(id);
+            return View(oLista);
+            
+        }
+    }
+}
---
+++ Progra2/Models/MovimientoModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Progra2.Models
+{
+    public class MovimientoModel
+    {
+        public int Id { get; set; }
+        public DateTime Fecha { get; set; }
+        [Range(0, int.MaxValue, ErrorMessage = "El monto debe ser un valor positivo.")]
+        public int Monto { get; set; }
+        public int NuevoSaldo { get; set; }
+        public string? NombreTipoMovimiento { get; set; }
+        public string? NombreUsuario { get; set; }
+        public string? PostIP { get; set; }
+        public DateTime PostTime { get; set; }
+        public EmpleadoModel? Empleado { get; set; } // Esta clase sirve para almacenar el Empleado correspondiente al Movimiento
+    }
+}
