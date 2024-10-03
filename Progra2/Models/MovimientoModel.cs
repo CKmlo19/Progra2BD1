90b55c43567547a860309d1c827cb632b776197e
Camilo Orocú <luiscamilooc@gmail.com>
2024-10-02 22:28:26 -0600

    Creando la vista y el model para el insert de movimientos

--- Progra2/Models/MovimientoModel.cs
+++ Progra2/Models/MovimientoModel.cs
@@ -5,14 +5,13 @@ namespace Progra2.Models
     public class MovimientoModel
     {
         public int Id { get; set; }
-        public DateTime Fecha { get; set; }
-        [Range(0, int.MaxValue, ErrorMessage = "El monto debe ser un valor positivo.")]
+        public int IdEmpleado { get; set; }
+        public int IdTipoMovimieto { get; set; }
         public int Monto { get; set; }
-        public int NuevoSaldo { get; set; }
         public string? NombreTipoMovimiento { get; set; }
-        public string? NombreUsuario { get; set; }
         public string? PostIP { get; set; }
         public DateTime PostTime { get; set; }
         public EmpleadoModel? Empleado { get; set; } // Esta clase sirve para almacenar el Empleado correspondiente al Movimiento
+        public List<TipoMovimientoModel>? TipoMovimieto { get;} // Para mostrar los tipos de movimientos para insertar
     }
 }
